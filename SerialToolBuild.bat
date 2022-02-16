d055d04288e597f401e814e84b182052901a0666
x <2307230673@qq.com>
2022-02-16 16:24:05 +0800

    [new] 使用脚本生成可执行程序，采用pyinstaller -D选项生成文件夹（加快程序启动速度），并复制所需的图片

---
+++ SerialToolBuild.bat
@@ -0,0 +1,16 @@
+::create 2/15/2022
+
+set New_Name=Serial_Tool
+::编译输出到文件夹，加速exe文件启动速度
+pyinstaller -D .\main.py -n %New_Name%
+
+::生成exe文件，运行期间无命令行窗口
+::pyinstaller -F -w .\main.py -n %New_Name%
+
+echo New_Name is %New_Name%
+
+::创建文件夹
+md .\dist\%New_Name%\Logo_Picture\
+::复制文件夹下所有文件
+copy .\Logo_Picture\* .\dist\%New_Name%\Logo_Picture\
+
